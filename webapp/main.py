4fce051c60a7ad299ad01316c6cf5852f1c1925f
Gemeng <qingemeng@gmail.com>
2018-12-16 01:25:59 +0800

    Add bootstrap

--- webapp/main.py
+++ webapp/main.py
@@ -67,6 +67,7 @@ def retrieve():
         <html lang="en">
         <head>
             <meta charset="UTF-8">
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         </head>
         <body>    
             <form method="get" action="/search">
@@ -76,7 +77,7 @@ def retrieve():
             {table}
         </body>
         </html>
-        '''.format(table=table)
+        '''.format(table=table, css_file= url_for('static'))
     return render_template_string(search_template)
 
 
