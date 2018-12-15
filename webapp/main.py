fa1b82d51d866322d2b07dcb106d719c7c553dcb
qingemeng <qingemeng@gmail.com>
2018-12-15 15:26:16 +0000

    Fix minor issue

--- webapp/main.py
+++ webapp/main.py
@@ -31,7 +31,7 @@ def rank(input, ref_df):
 
 def search(se, ref_df, query):
     idxs, dists = se.search(query)
-    ranked_results = sorted(zip(idxs, dists), key=rank)
+    ranked_results = sorted(zip(idxs, dists), key=lambda input: rank(input, ref_df))
     for idx, dist in ranked_results[:10]:
         content = ref_df.iloc[idx].content
         url = ref_df.iloc[idx].url
@@ -51,12 +51,11 @@ def hello():
 
 
 @app.route('/search')
-def search():
+def retrieve():
     query_string = request.args.get('query_string')
     search(search_engine, ref_df, query_string)
 
 
 if __name__ == '__main__':
     ref_df, search_engine = init_search_engine()
-
     app.run(port=5001, host='0.0.0.0')
