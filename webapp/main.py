c474e1fe4d94dedc915ed7aa97e55035dfc3ed3a
Gemeng <qingemeng@gmail.com>
2018-12-15 22:58:49 +0800

    Build web app for retrieve result

---
+++ webapp/main.py
@@ -0,0 +1,62 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_app]
+from flask import Flask, request
+from search_engine import init_search_engine
+
+app = Flask(__name__)
+ref_df = 'global'
+search_engine = 'global'
+
+
+def rank(input, ref_df):
+    idx, dist = input
+    vote = ref_df.iloc[idx].vote
+    polarity = ref_df.iloc[idx].sentiment_polarity
+    subjectivity = ref_df.iloc[idx].sentiment_subjectivity
+    return dist + 0.01 / vote + 0.001 / polarity
+
+
+def search(se, ref_df, query):
+    idxs, dists = se.search(query)
+    ranked_results = sorted(zip(idxs, dists), key=rank)
+    for idx, dist in ranked_results[:10]:
+        content = ref_df.iloc[idx].content
+        url = ref_df.iloc[idx].url
+        vote = ref_df.iloc[idx].vote
+        polarity = ref_df.iloc[idx].sentiment_polarity
+        subjectivity = ref_df.iloc[idx].sentiment_subjectivity
+        score = dist + 0.01 / vote + 0.001 / polarity
+        print(content)
+        print(f'score: {score}')
+        print(f'url: {url}\n---------------\n')
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!!!'
+
+
+@app.route('/search')
+def search():
+    query_string = request.args.get('query_string')
+    search(search_engine, ref_df, query_string)
+
+
+if __name__ == '__main__':
+    ref_df, search_engine = init_search_engine()
+
+    app.run(port=5001, host='0.0.0.0')
---
+++ webapp/search_engine.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+import pandas as pd
+import torch
+import numpy as np
+from sklearn import preprocessing
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'notebooks'))
+import lang_model_utils
+
+class search_engine:
+    """Organizes all the necessary elements we need to make a search engine."""
+
+    def __init__(self,
+                 nmslib_index,
+                 ref_df,
+                 query2emb_func):
+        """
+        Parameters
+        ==========
+        nmslib_index : nmslib object
+            This is pre-computed search index.
+        ref_df : pandas.DataFrame
+            This dataframe contains meta-data for search results,
+            must contain the columns 'code' and 'url'.
+        query2emb_func : callable
+            This is a function that takes as input a string and returns a vector
+            that is in the same vector space as what is loaded into the search index.
+
+        """
+        assert 'url' in ref_df.columns
+        assert 'content' in ref_df.columns
+
+        self.search_index = nmslib_index
+        self.ref_df = ref_df
+        self.query2emb_func = query2emb_func
+
+    def search(self, str_search, k=1000):
+        """
+        Prints the code that are the nearest neighbors (by cosine distance)
+        to the search query.
+
+        Parameters
+        ==========
+        str_search : str
+            a search query.  Ex: "read data into pandas dataframe"
+        k : int
+            the number of nearest neighbors to return.  Defaults to 2.
+
+        """
+        query = self.query2emb_func(str_search)
+        return self.search_index.knnQuery(query, k=k)
+
+
+def init_search_engine():
+    input_path = Path('../notebooks/data/stackoverflow/processed_data/')
+    body_df = pd.read_csv(input_path / 'test.content_token', header=None, names=['content'])
+    vote_df = pd.read_csv(input_path / 'test.vote', header=None, names=['vote'])
+    assert body_df.shape[0] == vote_df.shape[0]
+    comment_df = pd.read_csv(input_path / 'test.comment')
+    assert body_df.shape[0] == comment_df.shape[0]
+    url_df = pd.read_csv(input_path / 'test.url', header=None, names=['url'])
+    assert body_df.shape[0] == url_df.shape[0]
+    ref_df = pd.concat([url_df, body_df, comment_df, vote_df], axis=1).reset_index(drop=True)
+    ref_df.vote = np.tanh(ref_df.vote)
+    ref_df[['sentiment_polarity', 'sentiment_subjectivity']] = preprocessing.MinMaxScaler().fit_transform(ref_df[['sentiment_polarity', 'sentiment_subjectivity']])
+
+    # read data
+    with open(input_path / 'train.content_token', 'r') as f:
+        trn_raw = f.readlines()
+
+    with open(input_path / 'valid.content_token', 'r') as f:
+        val_raw = f.readlines()
+
+    with open(input_path / 'test.content_token', 'r') as f:
+        test_raw = f.readlines()
+
+    vocab = lang_model_utils.load_lm_vocab('./data/stackoverflow/lang_model/vocab.cls')
+    lang_model = torch.load('./data/stackoverflow/lang_model/lang_model_cpu.torch',
+                            map_location=lambda storage, loc: storage)
+    q2emb = lang_model_utils.Query2Emb(lang_model = lang_model.cpu(),
+                  vocab = vocab)
+
+    search_index = nmslib.init(method='hnsw', space='cosinesimil')
+    search_index.loadIndex('./data/stackoverflow/lang_model_emb/dim500_avg_searchindex.nmslib')
+
+    se = search_engine(nmslib_index=search_index,
+                       ref_df=ref_df,
+                       query2emb_func=q2emb.emb_mean)
+
+    return ref_df, se
