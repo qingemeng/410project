6d7227d249142793c14d731e026b8d1b5af1fb56
qingemeng <qingemeng@gmail.com>
2018-12-15 13:45:17 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# apt-get update && apt-get install vim
+# apt-get update && apt-get install vim man -y
 
 pip install --upgrade pip
 pip install tensorflow
@@ -8,3 +8,4 @@ pip install google-cloud-storage google-cloud-bigquery
 pip install --upgrade google-cloud-language
 pip install --upgrade tqdm
 pip install textblob
+pip install wtforms
